3a18b91447bca843ac3bd03b3007873fd9d37a4a
robelchane <robel.chane@edu.sait.ca>
2024-09-23 14:39:36 -0600

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -35,45 +35,44 @@ export default function Home() {
       </div>
 
       {/* Content Section */}
-      <div className="flex justify-center font-serif mt-20">
-        <div className="w-1/2 m-10">
-          <div className="font-bold text-black m-5 text-5xl gap-5">
-            <p>Discover</p>
-            <p className="my-2">Most Suitable</p>
-            <p>Property</p>
-
-          </div>
-          <div className="my-10 text-lg">
-            <p>Find a variety of properties that suit you very easilty
-            Forget all difficulties in finding a residence for you </p>
-          </div>
+      <div className="relative bg-cover bg-center text-black" 
+        style={{ backgroundImage: "url('/home.png')" }}>
           
-          <SearchBar />
+        <div className="flex justify-center font-serif mt-20">
+          <div className="m-10">
+            <div className="font-bold text-black m-5 text-5xl gap-5">
+              <p>Discover</p>
+              <p className="my-2">Most Suitable</p>
+              <p>Property</p>
+
+            </div>
+            <div className="my-10 text-lg font-bold">
+              <p>Find a variety of properties that suit you very easilty
+              Forget all difficulties in finding a residence for you </p>
+            </div>
+          
+            <SearchBar />
          
-          <div className="flex justify-between text-4xl mt-10">
-            <p>
-              9,000 <span className="text-orange-500">+</span>
-            </p>
-            <p>
-              2,000 <span className="text-orange-500">+</span>
-            </p>
-            <p>
-              28 <span className="text-orange-500">+</span>
-            </p>
-          </div>
+            <div className="flex justify-between text-4xl mt-10">
+              <p>
+                9,000 <span className="text-orange-500">+</span>
+              </p>
+              <p>
+                2,000 <span className="text-orange-500">+</span>
+              </p>
+              <p>
+                28 <span className="text-orange-500">+</span>
+              </p>
+            </div>
 
-          <div className="flex justify-between text-base mb-10">
-            <p>Premium Product</p>
-            <p>Happy Customer</p>
-            <p>Awards Winning</p>
+            <div className="flex justify-between text-base mb-10">
+              <p>Premium Product</p>
+              <p>Happy Customer</p>
+              <p>Awards Winning</p>
+            </div>
           </div>
-        </div>
-
-        <div className="w-1/3 m-10 ">
-          <img src={"/home.png"} alt="Profile picture" />
         </div>
       </div>
-
     </main>
   );
 }
