cc304450c74d0b7a2db62a5a2e4a888351b946b6
Gaon Sun <gaonseon@gmail.com>
2024-09-26 21:58:09 -0600

    update

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -6,17 +6,6 @@ import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-
 import Link from 'next/link';
 
 export default function Contact() {
-  const [showPopup, setShowPopup] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowPopup(true);
-  };
-
-  const closePopup = () => {
-    setShowPopup(false);
-  };
-
   return (
     <main className="font-serif overflow-y-auto text-black" id="contact">
       {/* Content Section */}
@@ -53,7 +42,7 @@ export default function Contact() {
             <Link href="/contactform">
               <button
                 type="button"
-                className="text-xl text-shadow center-align bg-blue-800 text-white p-4 rounded-lg hover:bg-blue-900"
+                className="text-xl text-shadow center-align bg-[#001f3f] text-white p-4 mt-10 rounded-lg hover:bg-blue-900 shadow-md hover:scale-105 transition-transform duration-300"
               >
                 Contact Us
               </button>
@@ -61,33 +50,11 @@ export default function Contact() {
           </div>
         </div>
 
-        {/* Right Contact Form */}
-        <div className="w-1/2 border border-gray-400 bg-gray-200 p-5 rounded-lg transition-all hover:p-6 hover:m-1">
-          <p className="text-xl font-bold mb-5 text-center">Leave your contact and message, we will contact you.</p>
-          
-          {/* Contact Form */}
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <input type="text" placeholder="Name" required className="border border-gray-300 p-3 rounded-lg" />
-            <input type="email" placeholder="Email Address" required className="border border-gray-300 p-3 rounded-lg" />
-            <input type="tel" placeholder="Phone Number" required className="border border-gray-300 p-3 rounded-lg" />
-            <textarea placeholder="Your Message" required className="border border-gray-300 p-3 rounded-lg h-32"></textarea>
-            <button type="submit" className="text-white p-3 bg-[#001f3f] rounded-lg transition-transform duration-300 ease-in-out hover:bg-blue-600 hover:text-white hover:scale-105">Send</button>
-          </form>
+        {/* Image Section */}
+        <div className="w-1/2 m-10">
+          <img src="/contact.jpg" alt="Contact Us"/>
         </div>
       </div>
-
-      {/* Popup Message */}
-      {showPopup && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-75">
-          <div className="bg-white p-5 rounded-lg shadow-lg relative">
-            <button onClick={closePopup} className="absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700">
-              <FontAwesomeIcon icon={faTimes} className="w-5" />
-            </button>
-            <p className="text-lg">Your message has been successfully sent! We will get back to you soon.</p>
-          </div>
-        </div>
-      )}
     </main>
   );
 }
-
