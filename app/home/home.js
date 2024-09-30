5fdfd7882df839d168a0838340a0107c5b6c8b47
tanztanz <129794928+tanztanz@users.noreply.github.com>
2024-09-30 02:09:03 -0600

    got rid of the header lining at the top of the image and set static bg image

--- app/home/home.js
+++ app/home/home.js
@@ -5,60 +5,59 @@ import SearchBar from "./searchBar";
 
 export default function Home() {
   return (
-    <main className="font-serif overflow-y-auto text-white">
-      {/* Background Image covering the entire main section */}
-      <div 
-        className="relative w-full h-screen bg-cover bg-center"
-        style={{ 
-          backgroundImage: `url(/background.jpg)`, 
-          backgroundColor: 'rgba(0, 0, 0, 0.7)' 
-        }}
-      >
-        {/* Fixed Header */}
-        <div className="fixed top-0 left-0 right-0 z-50 shadow-none p-3 bg-transparent">
-          <div className="flex justify-between text-xl font-serif">
-            <div className="m-2">
-              <p className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-400 tracking-widest uppercase">
-                Property Pros
+    <main className="font-serif overflow-y-auto bg-gray-900 text-white">
+      {/* Fixed Header */}
+      <div className="fixed top-0 left-0 right-0 z-50 p-3">
+        <div className="flex justify-between text-xl font-serif text-white">
+          <div className="m-2">
+            <p className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-400 tracking-widest uppercase">
+              Property Pros
+            </p>
+          </div>
+          <div className="flex gap-10 m-2">
+            <Link href="/listings">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Listings
               </p>
-            </div>
-            <div className="flex gap-10 m-2">
-              <Link href="/listings">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Listings
-                </p>
-              </Link>
-              <Link href="/seller">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Seller
-                </p>
-              </Link>
-              <Link href="#residencies">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Residencies
-                </p>
-              </Link>
-              <Link href="#value">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Our Values
-                </p>
-              </Link>
-              <Link href="#contact">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Contact Us
-                </p>
-              </Link>
-              <Link href="/login">
-                <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
-                  Get Started
-                </p>
-              </Link>
-            </div>
+            </Link>
+            <Link href="/seller">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Seller
+              </p>
+            </Link>
+            <Link href="#residencies">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Residencies
+              </p>
+            </Link>
+            <Link href="#value">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Our Values
+              </p>
+            </Link>
+            <Link href="#contact">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Contact Us
+              </p>
+            </Link>
+            <Link href="/login">
+              <p className="transition-transform transform hover:scale-110 hover:text-transparent hover:underline font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-yellow-300">
+                Get Started
+              </p>
+            </Link>
           </div>
         </div>
+      </div>
 
-        {/* Content Section over the Background */}
-        <div className="flex justify-center items-center relative h-full mt-20">
+      {/* Content Section with Static Background Image */}
+      <div className="flex justify-center items-center font-serif relative h-screen">
+        <div 
+          className="relative w-full h-full bg-cover bg-center" 
+          style={{ 
+            backgroundImage: `url(/background.jpg)`,
+            backgroundColor: 'rgba(0, 0, 0, 0.7)' // Dark overlay for the background image
+          }}
+        >
           {/* Content over the background */}
           <div className="relative flex flex-col items-center justify-center w-full h-full z-10">
             <div className="font-bold text-white text-6xl gap-5 text-center">
@@ -88,6 +87,42 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* About Us Section */}
+      <div className="mt-20 flex justify-center font-serif bg-white text-black p-10">
+        <div className="w-1/2 m-10">
+          <img src={"/aboutus.png"} alt="About Us" className="mt-5"/>
+        </div>
+
+        <div className="flex flex-col w-1/2 m-10">
+          <div className="flex flex-col items-start">
+            <span className="text-3xl font-bold mt-5">Find the best home for all your needs</span>
+          </div>
+          <div className="my-10 text-lg">
+            <p className="mb-5">
+              With over 20 years of experience in the real estate industry, Property Pros is dedicated to
+              helping clients navigate the complexities of buying and selling homes. Our team of specialists
+              is committed to providing personalized service, ensuring that each client's unique needs and aspirations
+              are prioritized throughout the entire process. We believe that open communication and transparency are essential
+              in building lasting relationships with our clients, allowing us to deliver exceptional results tailored to their specific goals.
+            </p>
+            <p className="mb-10">
+              Our team's extensive knowledge and expertise set us apart in the competitive real estate landscape.
+              With a focus on data-driven decision-making, we conduct thorough market analyses and valuations to ensure
+              our clients make informed choices. Whether buying or selling, clients can trust that Property Pros will guide
+              them with integrity, diligence, and a relentless drive for success.
+            </p>
+            <span className="text-3xl font-bold">We're Here for You</span>
+            <p className="mt-10">
+              At Property Pros, we are committed to being your trusted partner throughout your real estate journey.
+              With us, you can feel confident that your needs are our top priority, ensuring a seamless and positive experience
+              as you achieve your real estate goals.
+            </p>
+          </div>
+        </div>    
+      </div>
+
+      {/* Calgary Market Analysis Section */}
     </main>
   );
 }
