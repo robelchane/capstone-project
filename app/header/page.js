e8aaa30a98ceeb58406c774518c61c9afd80dfd6
miebaka94 <semenitari@icloud.com>
2024-10-17 08:56:07 -0600

    added drop down and renamed headers
    1. Merged Listings and Seller to Properties and added dropdown for buyer and seller
    2. Added Discover dropdown that links to about us page
    3. Added "sign-in and sign-up dropdown to get started

--- app/header/page.js
+++ app/header/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import Link from "next/link";
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import { FaUser } from 'react-icons/fa'; // Import the user icon from Font Awesome
 import { auth } from '../firebase/firebase'; // Import the Firebase auth instance directly
 import { onAuthStateChanged } from "firebase/auth"; // Import onAuthStateChanged from Firebase Auth
@@ -16,7 +16,6 @@ const Header = () => {
   const router = useRouter(); // Ensure useRouter is at the top, in the client-side component
   const [scrollY, setScrollY] = useState(0);
   const [user, setUser] = useState(null);
-
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -48,9 +47,6 @@ const Header = () => {
     };
   }, []);
 
-  // Firebase Authentication State Change
-  
-
   return (
     <div className={`fixed top-0 left-0 right-0 z-50 p-4 transition-colors duration-500 ${scrollY > 0 ? 'bg-black bg-opacity-90' : 'bg-transparent'}`}>
       <div className="flex justify-between text-xl font-serif text-white">
@@ -69,40 +65,101 @@ const Header = () => {
           </Link>
         </div>
         <div className="flex gap-10 m-2">
-          <Link href="/listings">
-            <p className="text-white text-shadow hover:scale-110 transition-transform duration-300 text-xl">Listings</p>
-          </Link>
-          <Link href="/seller">
-            <p className="text-white text-shadow hover:scale-110 transition-transform duration-300 text-xl">Seller</p>
-          </Link>
-          <Link href="#residencies">
-            <p className="text-white text-shadow hover:scale-110 transition-transform duration-300 text-xl">Residencies</p>
-          </Link>
-          <Link href="#value">
-            <p className="text-white text-shadow hover:scale-110 transition-transform duration-300 text-xl">Our Values</p>
-          </Link>
 
-         {/* Conditional Rendering based on login status */}
-         {user ? (
+          {/* Dropdown Menu for Properties */}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <p className="cursor-pointer text-white text-xl font-medium transition-transform duration-300 hover:scale-105">
+                Properties
+              </p>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mt-2 w-48 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-400 transition-colors rounded-lg" 
+                onClick={() => router.push('/seller')}
+              >
+                Seller
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-400 transition-colors rounded-lg" 
+                onClick={() => router.push('/listings')}
+              >
+                Buyer
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Discover Dropdown Menu */}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <p className="cursor-pointer text-white text-xl font-medium transition-transform duration-300 hover:scale-105">
+                Discover
+              </p>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mt-2 w-48 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-400 transition-colors rounded-lg" 
+                onClick={() => router.push('/about')}
+              >
+                About Us
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-400 transition-colors rounded-lg" 
+                onClick={() => router.push('/living-in-calgary')}
+              >
+                Living in Calgary
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Get Started Dropdown Menu */}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <p className="cursor-pointer text-white text-xl font-medium transition-transform duration-300 hover:scale-105">
+                Get Started
+              </p>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mt-2 w-48 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-400 transition-colors rounded-lg" 
+                onClick={() => router.push('/login')}
+              >
+                Sign In
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-00 transition-colors rounded-lg" 
+                onClick={() => router.push('/signup')}
+              >
+                Sign Up
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Conditional Rendering based on login status */}
+          {user ? (
             // If user is logged in, show the user icon with a dropdown menu
             <DropdownMenu>
               <DropdownMenuTrigger>
                 <div className="flex items-center">
-                  
                   <FaUser className="text-white text-2xl cursor-pointer hover:scale-110 transition-transform duration-300" />
                 </div>
               </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem onClick={() => router.push('/account')}>Account</DropdownMenuItem>
-                <DropdownMenuItem onClick={signOut}>Logout</DropdownMenuItem>
+              <DropdownMenuContent className="mt-2 w-48 bg- rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <DropdownMenuItem 
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200 transition-colors rounded-lg" 
+                  onClick={() => router.push('/account')}
+                >
+                  Account
+                </DropdownMenuItem>
+                <DropdownMenuItem 
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200 transition-colors rounded-lg" 
+                  onClick={signOut}
+                >
+                  Logout
+                </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-          ) : (
-            // If user is not logged in, show "Get Started" button
-            <Link href="/login">
-              <p className="text-white text-shadow hover:scale-110 transition-transform duration-300 text-xl">Get Started</p>
-            </Link>
-          )}
+          ) : null}
         </div>
       </div>
     </div>
