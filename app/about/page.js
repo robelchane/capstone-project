89745e0c2c52f401b72a1a2063c4a5c7f190259b
miebaka94 <semenitari@icloud.com>
2024-10-18 00:11:04 -0600

    Updated about us
    changed name

--- app/about/page.js
+++ app/about/page.js
@@ -32,7 +32,7 @@ const About = () => {
           <h2 className="text-2xl font-semibold text-gray-800">Robel Chane</h2>
           <p className="text-gray-600">...</p>
           <p className="mt-2 text-gray-700">
-            Jane oversees all marketing initiatives at Property Pros, ensuring 
+            Robel oversees all marketing initiatives at Property Pros, ensuring 
             our listings reach the right audience and our brand is well represented.
           </p>
         </div>
