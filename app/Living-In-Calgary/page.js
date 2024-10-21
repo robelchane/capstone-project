d672d6b83e4478bd77977853d82efa1b474213a5
tanztanz <129794928+tanztanz@users.noreply.github.com>
2024-10-21 08:33:46 -0600

    fixed routing issue for living in calgary page

---
+++ app/Living-In-Calgary/page.js
@@ -0,0 +1,9 @@
+export default function LivingInCalgary() {
+    return (
+      <div>
+        <h1>Hello from Calgary!</h1>
+        <p>Welcome to the Living in Calgary page.</p>
+      </div>
+    );
+  }
+  
