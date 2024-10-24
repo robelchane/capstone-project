e68d0f440b057fc6233c0c9a68aa20bd4acd7175
miebaka94 <semenitari@icloud.com>
2024-10-24 15:57:30 -0600

    updated color
    made color of favorites to match "contact seller" button

--- app/listings/page.js
+++ app/listings/page.js
@@ -108,7 +108,7 @@ export default function Listings() {
                 <div className="flex justify-between items-center mt-4">
                   <button
                     onClick={() => handleAddToFavorites(residence)}
-                    className="bg-red-500 text-white px-4 py-2 rounded-lg"
+                    className="bg-yellow-700 text-white px-4 py-2 rounded-lg"
                   >
                     Add to Favorites
                   </button>
