6f2a918e183fbe86a9c8e9178adee979640e2a1b
robelchane <141067982+robelchane@users.noreply.github.com>
2024-10-29 22:18:14 -0600

    Update page.js

--- app/all-listings/page.js
+++ app/all-listings/page.js
@@ -173,7 +173,7 @@ export default function AllListings() {
                   onClick={() => toggleSaveProperty(property._id)}
                   className={`text-xl ${
                     savedProperties.has(property._id)
-                      ? "text-red-800"
+                      ? "text-red-500"
                       : "text-gray-500"
                   }`}
                 >
