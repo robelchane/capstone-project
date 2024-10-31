680f2929a42f5efc480084c7f3bec5f08d1d4b67
robelchane <141067982+robelchane@users.noreply.github.com>
2024-10-30 23:53:04 -0600

    Update page.js

--- app/mortgage-calculator/page.js
+++ app/mortgage-calculator/page.js
@@ -157,7 +157,7 @@ export default function MortgageCalculator() {
 
         {/* Right Side - Summary Output */}
         <div className="w-1/2 p-4 border-l border-gray-300">
-          <h3 className="text-lg font-semibold flex justify-center">Mortgage Repayment Summary</h3>
+          <h3 className="text-2xl font-semibold flex justify-center mb-4">Mortgage Repayment Summary</h3>
           <p><strong>Mortgage Amount:</strong> ${mortgageAmount}</p>
           <p><strong>Down Payment:</strong> ${downPayment}</p>
           <p><strong>Payment ({paymentFrequency}):</strong> ${monthlyPayment}</p>
