1d8e4c8717263fa40b0567586ab31adc586df2d3
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-01 16:37:01 -0600

    Update page.js

--- app/profile/saved-properties/page.js
+++ app/profile/saved-properties/page.js
@@ -5,13 +5,13 @@ import { useEffect,useState } from "react";
 export default function SavedPropertiesPage() {
 
     const [savedProperties, setSavedProperties] = useState([]);
-    
+
 
 
 
 
     return(
-        <div>
+        <div className="mt-4 flex justify-center ">
             <h1 className="text-3xl font-bold">Saved Properties list</h1>
             <p>View your saved properties here.</p>
         </div>
