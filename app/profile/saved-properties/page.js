0e7be9a0ea8bdf553836c25669d1e2974c103272
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-01 16:35:30 -0600

    Update page.js

--- app/profile/saved-properties/page.js
+++ app/profile/saved-properties/page.js
@@ -1,6 +1,10 @@
+'use client';
+import { useEffect,useState } from "react";
 
 
 export default function SavedPropertiesPage() {
+
+
     return(
         <div>
             <h1 className="text-3xl font-bold">Saved Properties list</h1>
