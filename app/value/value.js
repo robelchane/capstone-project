3b1c4ff6280efef1f387e82f666ed3d33a9e7219
Gaon Sun <gaonseon@gmail.com>
2024-11-06 01:01:57 -0700

    Update

--- app/value/value.js
+++ app/value/value.js
@@ -42,7 +42,7 @@ export default function Value() {
           <div className="text-center">
             <h2
               onClick={toggleDropdown1}
-              className= "cursor-pointer text-white font-semibold bg-gray-900 p-6 mt-5 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
+              className= "rounded cursor-pointer text-white text-xl bg-gray-900 p-6 mt-5 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
             >
               Best interest rates on the market
             </h2>
@@ -61,7 +61,7 @@ export default function Value() {
           <div className="text-center">
             <h2
               onClick={toggleDropdown2}
-              className="cursor-pointer text-white font-semibold bg-gray-900 p-6 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
+              className="rounded cursor-pointer text-white text-xl bg-gray-900 p-6 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
             >
               Easy and flexible payments
             </h2>
@@ -79,7 +79,7 @@ export default function Value() {
           <div className="text-center">
             <h2
               onClick={toggleDropdown3}
-              className="cursor-pointer text-white font-semibold bg-gray-900 p-6 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
+              className="rounded cursor-pointer text-white text-xl bg-gray-900 p-6 mb-5 shadow-md hover:bg-gray-700 hover:scale-105 transition-transform duration-300"
             >
               Premium customer support
             </h2>
