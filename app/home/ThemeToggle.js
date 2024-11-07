5ee89bb05c62e3135906dbe02dab282131ba5c33
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-07 11:34:50 -0700

    Create ThemeToggle.js

---
+++ app/home/ThemeToggle.js
@@ -0,0 +1,6 @@
+'use client';
+import { useState,useEffect } from 'react';
+import {FaMoon} from 'react-icons/fa';
+import {BSSunFill} from 'react-icons/fa';
+
+export default function ThemeToggle() {}
