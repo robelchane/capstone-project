a21fc9238211b798bd8ffef101aeebc1f4ffd4c7
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-07 11:37:50 -0700

    Update ThemeToggle.js

--- app/home/ThemeToggle.js
+++ app/home/ThemeToggle.js
@@ -1,3 +1,4 @@
+//ref https://www.youtube.com/watch?v=L7E3XEncsqI
 'use client';
 import { useState,useEffect } from 'react';
 import {FaMoon} from 'react-icons/fa';
@@ -5,6 +6,14 @@ import {BSSunFill} from 'react-icons/fa';
 
 export default function ThemeToggle() {
     const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        const theme = localStorage.getItem('theme');
+        if(theme === 'dark'){
+            setDarkMode(true);
+        }
+    
+    })
 
 
     return(
