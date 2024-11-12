36f4beecf1f995019731c5ed209a1871ab1ac8b0
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-11 23:06:24 -0700

    Update page.js

--- app/all-listings/[id]/page.js
+++ app/all-listings/[id]/page.js
@@ -1,7 +1,6 @@
 // Reference
 // https://webdev2.warsylewicz.ca/week-8/fetching-data
 // https://www.mongodb.com/docs/manual/reference/operator/query/
-// https://rajasekar.dev/blog/api-design-filtering-searching-sorting-and-pagination
 
 "use client";
 import { useState, useEffect } from "react";
