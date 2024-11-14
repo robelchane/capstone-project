7d60ceeeacae095d67cbb267fa50593e68b37d48
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-14 14:40:01 -0700

    Update value.js

--- app/value/value.js
+++ app/value/value.js
@@ -61,7 +61,7 @@ export default function Value() {
           <div className="text-center">
             <h2
               onClick={toggleDropdown2}
-              className="rounded cursor-pointer text-white text-xl bg-black p-6 mb-5 shadow-md hover:scale-105 transition-transform duration-300"
+              className="rounded cursor-pointer text-white text-xl rounded-full bg-black p-6 mb-5 shadow-md hover:scale-105 transition-transform duration-300"
             >
               Easy and flexible payments
             </h2>
@@ -79,7 +79,7 @@ export default function Value() {
           <div className="text-center">
             <h2
               onClick={toggleDropdown3}
-              className="rounded cursor-pointer text-white text-xl bg-black p-6 mb-5 shadow-md hover:scale-105 transition-transform duration-300"
+              className="rounded cursor-pointer text-white text-xl rounded-full bg-black p-6 mb-5 shadow-md hover:scale-105 transition-transform duration-300"
             >
               Premium customer support
             </h2>
