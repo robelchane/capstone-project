1ad2e8c17216586de865740b9ead9626c5678c91
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-15 16:42:30 -0700

    Update page.js

--- app/all-listings/page.js
+++ app/all-listings/page.js
@@ -7,12 +7,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSearch,
-  faBed,
-  faBath,
-  faHeart,
-} from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faBed, faBath, faHeart } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export default function AllListings() {
@@ -87,9 +82,9 @@ export default function AllListings() {
   };
 
   return (
-    <div className="mt-44 px-4 justify-items-center items-center">
-      <h1 className="text-5xl text-[#001f3f] font-serif dark:text-white mb-10 text-center">Iconic for good reason</h1>
-      <form onSubmit={handleSearch} className="grid grid-cols-5 gap-5 mb-24 max-w-6xl">
+    <div className="p-12 py-8 px-10 mt-24">
+      <h1 className="text-2xl font-bold mb-4">Filter Properties</h1>
+      <form onSubmit={handleSearch} className="grid grid-cols-5 gap-4 mb-8">
         <div>
           <input
             name="minPrice"
@@ -97,7 +92,7 @@ export default function AllListings() {
             placeholder="Min Price"
             value={filters.minPrice}
             onChange={handleChange}
-            className="border p-2 w-full"
+            className="border p-2 w-full rounded-full"
           />
         </div>
         <div>
@@ -107,7 +102,7 @@ export default function AllListings() {
             placeholder="Max Price"
             value={filters.maxPrice}
             onChange={handleChange}
-            className="border p-2 w-full"
+            className="border p-2 w-full rounded-full"
           />
         </div>
         <div>
@@ -117,7 +112,7 @@ export default function AllListings() {
             placeholder="Bedrooms"
             value={filters.bedrooms}
             onChange={handleChange}
-            className="border p-2 w-full"
+            className="border p-2 w-full rounded-full"
           />
         </div>
         <div>
@@ -127,7 +122,7 @@ export default function AllListings() {
             placeholder="Bathrooms"
             value={filters.bathrooms}
             onChange={handleChange}
-            className="border p-2 w-full"
+            className="border p-2 w-full rounded-full"
           />
         </div>
         <div>
@@ -137,13 +132,13 @@ export default function AllListings() {
             placeholder="Enter address..."
             value={filters.address}
             onChange={handleChange}
-            className="border p-2 w-full"
+            className="border p-2 w-full rounded-full"
           />
         </div>
-        <div className="col-span-5 flex justify-center">
+        <div className="col-span-5 flex justify-center items-center">
           <button
             type="submit"
-            className="mt-4 bg-[#001f3f] text-white border border-black px-4 py-2 hover:bg-transparent hover:text-[#001f3f] hover:border-[#001f3f] dark:border-white dark:text-white transition-colors duration-300"
+            className="bg-black text-white border border-white px-4 py-2 rounded hover:bg-transparent hover:text-black hover:border-black transition-colors duration-300 w-full"
           >
             <FontAwesomeIcon icon={faSearch} /> Search
           </button>
@@ -152,67 +147,54 @@ export default function AllListings() {
 
       {loading && <p>Loading properties...</p>}
 
-      <div className="mx-auto max-w-8xl px-16">
-        <div className="border-t border-gray-300"></div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16 mb-16 px-4 lg:px-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
         {properties.length > 0 ? (
           properties.map((property) => (
-            <Link href={`/all-listings/${property._id}`} key={property._id}>
-              <div className="p-4 border rounded shadow-md cursor-pointer">
+            <div key={property._id} className="p-4 border rounded shadow-md">
+              <Link href={`/all-listings/${property._id}`}>
                 <img
                   src={`/images/${property.image}`}
                   alt={property.name}
-                  className="w-full h-48 object-cover mb-4"
+                  className="w-full h-48 object-cover mb-4 cursor-pointer"
                 />
-                <h2 className="text-xl font-bold mb-2">{property.name}</h2>
-                <p className="text-lg text-gray-700 dark:text-white">
-                  <span style={{ color: "#001f3f" }}>$</span>
-                  {property.price}
-                </p>
-
-                <p className="text-gray-600 dark:text-white">
-                  {property.summary}
-                </p>
-                <p className="text-sm text-gray-500 dark:text-white">
-                  {property.address}
-                </p>
-                <div className="flex items-center mt-2">
-                  <FontAwesomeIcon
-                    icon={faBed}
-                    className="text-gray-600 mr-1"
-                  />
-                  <span>{property.bedrooms} Bedrooms</span>
-                  <FontAwesomeIcon
-                    icon={faBath}
-                    className="text-gray-600 mx-2"
-                  />
-                  <span>{property.bathrooms} Bathrooms</span>
-                </div>
-                <div className="flex items-center justify-between mt-4">
-                  <p className="text-sm text-gray-500">
-                    Seller:{" "}
-                    <a
-                      href={`mailto:${property.sellerEmail}`}
-                      className="text-blue-500 hover:underline"
-                    >
-                      {property.sellerName} ({property.sellerEmail})
-                    </a>
-                  </p>
-                  <button
-                    onClick={() => toggleSaveProperty(property._id)}
-                    className={`text-xl ${
-                      savedProperties.has(property._id)
-                        ? "text-red-500"
-                        : "text-gray-500"
-                    }`}
-                  >
-                    <FontAwesomeIcon icon={faHeart} />
-                  </button>
-                </div>
+                <h2 className="text-xl font-bold mb-2 cursor-pointer">
+                  {property.name}
+                </h2>
+              </Link>
+              <p className="text-lg text-gray-700 dark:text-white">
+                <span style={{ color: "#001f3f" }}>$</span>
+                {property.price}
+              </p>
+              <p className="text-gray-600 dark:text-white">{property.summary}</p>
+              <p className="text-sm text-gray-500 dark:text-white">{property.address}</p>
+              <div className="flex items-center mt-2">
+                <FontAwesomeIcon icon={faBed} className="text-gray-600 mr-1" />
+                <span>{property.bedrooms} Bedrooms</span>
+                <FontAwesomeIcon icon={faBath} className="text-gray-600 mx-2" />
+                <span>{property.bathrooms} Bathrooms</span>
               </div>
-            </Link>
+              <div className="flex items-center justify-between mt-4">
+                <p className="text-sm text-gray-500">
+                  Seller:{" "}
+                  <a
+                    href={`mailto:${property.sellerEmail}`}
+                    className="text-blue-500 hover:underline"
+                  >
+                    {property.sellerName} ({property.sellerEmail})
+                  </a>
+                </p>
+                <button
+                  onClick={() => toggleSaveProperty(property)}
+                  className={`text-xl ${
+                    savedProperties.has(property._id)
+                      ? "text-red-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  <FontAwesomeIcon icon={faHeart} />
+                </button>
+              </div>
+            </div>
           ))
         ) : (
           <p>No properties found.</p>
