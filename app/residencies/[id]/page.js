607284da5b8a69caedd8aaaf138a9b770f6d91c7
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-14 17:25:17 -0700

    Update

--- app/residencies/[id]/page.js
+++ app/residencies/[id]/page.js
@@ -79,15 +79,15 @@ export default function PropertyPage({ params }) {
           </div>
 
           <div className="font-semibold flex gap-2 items-center bg-green-600 rounded-full p-3 justify-center">
-            <span className="text-black">Built In: {property.yearBuilts}</span>
+            <span className="text-black">Built In: {property.yearBuilt}</span>
           </div>
 
           <div className="font-semibold flex gap-2 items-center bg-green-600 rounded-full p-3 justify-center">
-            <span className="text-black">Parking: {property.sqft}</span>
+            <span className="text-black">Parking: {property.parking}</span>
           </div>
 
           <div className="font-semibold flex gap-2 items-center bg-green-600 rounded-full p-3 justify-center">
-            <span className="text-black">Sqft: {property.sqft}</span>
+            <span className="text-black">Property Type: {property.propertyType}</span>
           </div>
           
 
