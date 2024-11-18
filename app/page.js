69ee47dbb120cc42a9845647e1d88f08e1744b6b
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-18 08:31:38 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Home from "./home/home.js";
 import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
 import CustomerReview from "./customerReview/CustomerReview.js";
+import ContactUs from "./contact-us/page.js"
 
 export default function Page() {
   return (
@@ -11,7 +12,8 @@ export default function Page() {
       <Home />
       <Residencies />
       <Value />
-      <CustomerReview />     
+      <CustomerReview />
+      <ContactUs />     
     </main>
   );
 }
