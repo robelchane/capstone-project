5fcba98b143e6e365c7e1ba42ed31f50804a284a
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-17 21:00:31 -0700

    Update

--- app/page.js
+++ app/page.js
@@ -4,17 +4,14 @@ import Home from "./home/home.js";
 import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
 import CustomerReview from "./customerReview/CustomerReview.js";
-import {ThemeProvider} from "next-themes";
 
 export default function Page() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
     <main className="bg-white">
       <Home />
       <Residencies />
       <Value />
       <CustomerReview />     
     </main>
-    </ThemeProvider>
   );
 }
