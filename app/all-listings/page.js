08ee254d8ad5454bba2132867d7761de2a04f60b
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2024-11-20 20:54:40 -0700

    Update page.js

--- app/all-listings/page.js
+++ app/all-listings/page.js
@@ -138,7 +138,7 @@ export default function AllListings() {
         <div className="col-span-5 flex justify-center items-center">
           <button
             type="submit"
-            className="bg-black text-white border border-white px-4 py-2 rounded hover:bg-transparent hover:text-black hover:border-black transition-colors duration-300 w-full"
+            className="bg-black rounded-full text-white border border-white px-4 py-2 rounded hover:bg-transparent hover:text-black hover:border-black transition-colors duration-300 w-full"
           >
             <FontAwesomeIcon icon={faSearch} /> Search
           </button>
