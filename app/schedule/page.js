9cbe8db3b4cb82255411a2c475b08ff8d6a69e8e
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-27 22:10:13 -0700

    Update page.js

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function Appointments() {
+export default function Schedule() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
