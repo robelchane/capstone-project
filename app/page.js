b8db8ba6d652f45991c246b4aff6f04d544875f7
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-28 12:57:15 -0700

    update

--- app/page.js
+++ app/page.js
@@ -3,12 +3,15 @@ import Home from "./home/home.js";
 import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
 import CustomerReview from "./customerReview/CustomerReview.js";
+import Bookings from "./bookings/page.js";
 
 export default function Page() {
   return (
     <main className="bg-white">
+    
       <Home />
       <Residencies />
+      <Bookings />
       <Value />
       <CustomerReview />
       <Footer />     
