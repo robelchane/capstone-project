9ecb77ef6032b5c6777e09f6b5906bc76ef2f123
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-27 21:23:54 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -1,5 +1,5 @@
 import dbConnect from "@/libs/mongodb";
-import Appointment from "@/models/Appointment";
+import Appointment from "../../../models/Appointment";
 
 export default async function handler(req, res) {
   await dbConnect();
