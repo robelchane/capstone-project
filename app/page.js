3738585171132c0e2130165e962c8b529d1da9d3
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-28 13:03:47 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
-import Footer from "./footer/footer.js";
+//ref: https://www.youtube.com/watch?v=FCCQdM3ZSw4
+
 import Home from "./home/home.js";
 import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
 import CustomerReview from "./customerReview/CustomerReview.js";
-import Bookings from "./bookings/page.js";
+import ContactUs from "./contact-us/page.js";
 
 export default function Page() {
   return (
     <main className="bg-white">
-    
       <Home />
       <Residencies />
-      <Bookings />
       <Value />
       <CustomerReview />
-      <Footer />     
+      <ContactUs />     
     </main>
   );
 }
