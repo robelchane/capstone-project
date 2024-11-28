7a3d9b0516d956d5cc6b06295e4191563eed3f98
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-28 13:02:26 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
 import CustomerReview from "./customerReview/CustomerReview.js";
 import Bookings from "./bookings/page.js";
+import AllAppointments from "./all-appointmets/page.js"
 
 export default function Page() {
   return (
@@ -12,6 +13,7 @@ export default function Page() {
       <Home />
       <Residencies />
       <Bookings />
+      <AllAppointments />
       <Value />
       <CustomerReview />
       <Footer />     
