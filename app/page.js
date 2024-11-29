1f8b854228aaf9e89996fcb4057c1babe0e65c23
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-28 20:24:41 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+//ref: https://www.youtube.com/watch?v=FCCQdM3ZSw4
+
 import Home from "./home/home.js";
 import Residencies from "./residencies/residencies.js";
 import Value from "./value/value.js";
