9101952e5f0a5261b49f0d0a7e30dd585b3d9f53
miebaka94 <semenitari@icloud.com>
2024-11-28 20:42:56 -0700

    Update appointments.js

--- models/appointments.js
+++ models/appointments.js
@@ -1,1 +1,9 @@
 import mongoose from "mongoose";
+
+const AppointmentSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    date: String,
+    time: String,
+    notes: String,
+  });
