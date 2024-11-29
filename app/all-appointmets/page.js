8399a80e4315a9cb9d7c515d0d503cad400d97fb
robelchane <141067982+robelchane@users.noreply.github.com>
2024-11-28 17:44:25 -0700

    Update page.js

--- app/all-appointmets/page.js
+++ app/all-appointmets/page.js
@@ -37,7 +37,7 @@ export default function AllAppointments() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-300 py-10 px-6">
+    <div className="min-h-screen bg-gray-300 py-10 px-6 mt-20">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-4xl font-extrabold text-center text-white mb-10">All Appointments</h1>
         {loading ? (
