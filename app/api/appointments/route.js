047d5a09125107a670efdfac4f348a272009d061
miebaka94 <semenitari@icloud.com>
2024-11-28 22:00:51 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -28,6 +28,6 @@ export  async function GET() {
     try {
       await  connectMongoDB(); //Connect to the database
 
-      //Retrieve all appointments from
+      //Retrieve all appointments from the 
 
 
