f87f87fa0ab4039c33ca22ead2297b05fadda77e
miebaka94 <semenitari@icloud.com>
2024-11-28 22:27:11 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -45,7 +45,7 @@ export  async function GET() {
 // DELETE: Remove an appointment by ID
 export async function DELETE(request) {
     try {
-        const id = request.nextUrl.searchParams.get("id");
+        const id = request.nextUrl.searchParams.get("id"); //Get the appointment
     }
 
 
