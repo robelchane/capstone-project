063f907d23a603866e7f6278e6942ed270717f94
miebaka94 <semenitari@icloud.com>
2024-11-28 20:53:31 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -2,3 +2,6 @@
 import connectMongoDB from "../../../libs/mongodb";
 import Appointment from "../../../models/appointment"; // Ensure consistent capitalization for model import
 import { NextResponse } from "next/server";
+
+
+// POST: Create a new appointment
