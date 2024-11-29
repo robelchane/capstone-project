0959319f2d86bcc0e0e2f8eb4444a457ff2cb383
miebaka94 <semenitari@icloud.com>
2024-11-28 21:17:07 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -11,4 +11,4 @@ try {
     await connectMongoDB(); //Establish the MongoDB connection
 
     // Create a new appointment with provided details 
-    const  newAppointment = await
+    const  newAppointment = await Appointment
