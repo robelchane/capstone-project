6badad7c47c98371c5692c1cde7f034829d6f8de
miebaka94 <semenitari@icloud.com>
2024-11-28 22:45:54 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -64,6 +64,7 @@ export async function DELETE(request) {
     // Catch and report errors related to deletion
     return NextResponse.json(
         { error: "Failed to delete appointment", details: error.message },
+        { status:}
 
     )
 
