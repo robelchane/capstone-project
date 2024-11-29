570f2f9712acdf4c12001ad9dd7dc439a64eca5f
miebaka94 <semenitari@icloud.com>
2024-11-28 21:01:40 -0700

    Update route.js

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -8,4 +8,4 @@ import { NextResponse } from "next/server";
 
 try {
     const{ name, email, date, time, notes } = await request.json(); //Extract fields from the request
-    await connectMongoDB(); 
+    await connectMongoDB(); //Establish the
